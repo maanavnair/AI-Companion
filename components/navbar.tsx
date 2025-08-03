2e32ecb181b1ba17000c324e6ea73cf43696a03d
Maanav Nair <maanavnair20042@gmail.com>
2025-08-03 19:12:38 +0530

    sidebar: completed

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -8,6 +8,7 @@ import Link from "next/link";
 import { UserButton } from "@clerk/nextjs";
 import { Button } from "./ui/button";
 import { ModeToggle } from "./mode-toggle";
+import { MobileSidebar } from "./mobile-sideebar";
 
 const font = Poppins({
     weight: "600",
@@ -17,9 +18,9 @@ const font = Poppins({
 export const Navbar = () => {
 
     return(
-        <div className="fixed w-full z-50 flex justify-between items-center py-2 px-4 border-b border-primary/10 bg-secondary">
+        <div className="fixed w-full z-50 flex justify-between items-center py-2 px-4 border-b border-primary/10 bg-secondary h-16">
             <div className="flex items-center">
-                <Menu className="block md:hidden" />
+                <MobileSidebar />
                 <Link href="/">
                     <h1 className={cn(
                         "hidden md:block text-xl md:text-3xl font-bold text-primary",
